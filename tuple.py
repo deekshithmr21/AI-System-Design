4ec407c3907b654971cb13cf30dc7814424fa87e
deekshithmr21 <deekshithmr3@gmail.com>
2025-10-21 23:44:47 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,10 @@
+def my_tuple(item):
+
+ return item.__sizeof__() 
+
+
+
+item = (56, 35, 66, 23, 87,99,9765,80 ,                                                 99999)
+
+print(f"The size of the tuple is: {my_tuple(item)} bytes")
+
