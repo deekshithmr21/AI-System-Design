2e8b0a22ceb270efd31aa6f5eaf6a5d4752c6cd5
Deekshith M R <deekshithmr3@gmail.com>
2025-10-22 22:02:49 +0530

    FEAT: practice scripts

---
+++ conditional(1).py
@@ -0,0 +1,30 @@
+temperature = 99
+is_raining = False
+
+if temperature > 30:
+    print("It's a hot day!")
+elif temperature >= 20:
+    print("It's a pleasant day.")
+else:
+    print("It's a bit chilly.")
+
+if is_raining:
+    print("Remember your umbrella.")
+else:
+    print("No need for an umbrella today.")
+
+age = 17
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+grade = 85
+if grade >= 90:
+    print("Excellent!")
+elif grade >= 80:
+    print("Very good.")
+elif grade >= 70:
+    print("Good.")
+else:
+    print("Needs improvement.")
